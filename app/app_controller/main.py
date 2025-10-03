7dfc86e39808d09fc8f98b974ae66d2e83f81fcb
Klayver-Gabriel <avviolaouclassico@gmail.com>
2025-10-03 12:45:47 -0300

    Adição da capacidade de escolher o dataset a ser analisado e/ou salvar tabelas criadas a partir de filtros

--- app/app_controller/main.py
+++ app/app_controller/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 from flask import Flask,render_template
 
-
 app = Flask(__name__)
+
 from view_routes import *
-brute_rol = pd.read_csv("Banco de Ninjas.csv")
+
 if __name__ == "__main__":
     app.run(port= 50001)
 
--- app/app_controller/view_routes.py
+++ app/app_controller/view_routes.py
@@ -3,62 +3,75 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from flask import render_template,request
 
-from app.app_controller.main import app, brute_rol
+from app.app_controller.main import app
 
+@app.route("/data",methods = ["GET"])
+def read():
+    nome_dataset = request.form.get("dataset")
 
-@app.route("/",methods = ["GET"])
-def showDataset():
     # SHOW THE DATASET
-    return render_template("dataset.html",table = pd.read_csv("Banco de Ninjas.csv").to_html())
+    return render_template("dataset.html",table = pd.read_csv(nome_dataset+".csv").to_html())
 @app.route("/dataset",methods = ["POST"])
-def modifyRol():
-        # GETTING THE VALUES FROM THE REQUEST
-        element = request.form.get("element")
-        village = request.form.get("village")
-        nome = request.form.get("nome")
-        salario_str = request.form.get("salario")
-        rank = request.form.get("rank")
-        salario = float(salario_str)
-        id = request.form.get("CPF")
+def create():
+    nome_dataset = request.form.get("dataset")
 
-        # CREATE A ARRAY WITH THE VALUES OF THE NEW NINJA SIGN IN
-        a = np.array([nome,element,village,salario,rank,id])
-        # ARRAY TO GET THE LINE WITH THE SAME VALUES OF THE SIGNED ID
-        b = np.array(brute_rol.loc[brute_rol.ID == id])
+    # GETTING THE VALUES FROM THE REQUEST
+    brute_rol = pd.read_csv(str(nome_dataset) + ".csv")
 
-        if a.all() != b.all():
-            brute_rol.loc[int(brute_rol.index.max() + 1)] = a
-        else:
-            raise Exception("CADASTRO JA ENCONTRADO")
+    element = request.form.get("element")
+    village = request.form.get("village")
+    nome = request.form.get("nome")
+    salario_str = request.form.get("salario")
+    rank = request.form.get("rank")
+    salario = float(salario_str)
+    id = request.form.get("CPF")
 
-        brute_rol.to_csv("Banco de Ninjas.csv",index=False)
-        return f"Olá {nome}, registramos você na organização mundial dos ninjas"
-@app.route("/elementos",methods = ["GET"])
-def showElements():
-    query = request.form.get("element")
-    return render_template("dataset.html",table = brute_rol[brute_rol["Elemento"] == query].to_html())
+    # CREATE A ARRAY WITH THE VALUES OF THE NEW NINJA SIGN IN
+    a = np.array([nome,element,village,salario,rank,id])
+    # ARRAY TO GET THE LINE WITH THE SAME VALUES OF THE SIGNED ID
+    b = np.array(brute_rol.loc[brute_rol.ID == id])
+
+    if a.all() != b.all():
+        brute_rol.loc[int(brute_rol.index.max() + 1)] = a
+    else:
+       raise Exception("CADASTRO JA ENCONTRADO")
+
+    brute_rol.to_csv(nome_dataset+".csv",index=False)
+    return f"Olá {nome}, registramos você na organização mundial dos ninjas"
 @app.route("/histograma",methods = ["GET"])
 def plot():
+    nome_dataset = request.form.get("dataset")
+
+    brute_rol = pd.read_csv(str(nome_dataset) + ".csv")
+
     plt.hist(brute_rol["Rank"],bins= 20)
     plt.show()
     return "GRAFICO GERADO"
 @app.route("/linear_regression",methods = ["GET"])
 def linear_regression():
+    nome_dataset = request.form.get("dataset")
+
+    brute_rol = pd.read_csv(str(nome_dataset) + ".csv")
+
     # X IS OUR PREDICTIVE VARIABLE
-    x = np.array(brute_rol["Rank"].values,int)
+    x = np.array(brute_rol["Rank"].values, int)
     # Y IS OUR DEPENDENT VARIABLE
     y = np.array(brute_rol["Salario"].values)
     # BETAS ARE OUR LINEAR FUNCTION SLOPE AND INTERCEPT
-    betas = np.polyfit(x,y,1)
+    betas = np.polyfit(x, y, 1)
     reta = np.poly1d(betas)
 
     # WE SCATTER OUR DATA ALL ACROSS A CHART, THEN WE CROSS IT WITH OUR LINEAR REGRESSION FUNCTION STRAIGHT
-    plt.scatter(x,y)
-    plt.plot(x,reta(x))
+    plt.scatter(x, y)
+    plt.plot(x, reta(x))
     plt.show()
     return "GRAFICO GERADO"
 @app.route("/clear",methods =  ["PUT"])
 def clean():
+    nome_dataset = request.form.get("dataset")
+
+    brute_rol = pd.read_csv(str(nome_dataset) + ".csv")
+
     # DATA CLEANING BASED ON THEIR IDS
     brute_rol["ID"] = pd.to_numeric(brute_rol["ID"],errors="coerce")
     brute_rol["ID"].dropna(inplace=True)
@@ -68,14 +81,22 @@ def clean():
     # DATA CLEANING TO FIX INVALID INPUTS IN THE DATASET(SALARIO NEEDS TO BE A INVALID INTEGER)
     brute_rol["Salario"] = pd.to_numeric(brute_rol["Salario"],errors="coerce")
     brute_rol["Salario"] = brute_rol["Salario"].fillna(brute_rol["Salario"].mean())
-    brute_rol.to_csv("Banco de Ninjas.csv",index=False)
+    brute_rol.to_csv(nome_dataset+".csv",index=False)
 
     return "Vazios Limpos"
 
 @app.route("/filter",methods = ["GET"])
 def filter_choose():
+    nome_dataset = request.form.get("dataset")
+
+    brute_rol = pd.read_csv(str(nome_dataset) + ".csv")
+
     collum = request.form.get("Collum")
     query = request.form.get("Filter")
-    filter = brute_rol[brute_rol[collum]  == query]
-    print(filter)
-    return render_template("dataset.html",table = filter.to_html())
+    nome_filtro = request.form.get("Nome do filtro")
+    print(nome_filtro)
+    rol_filtrado = brute_rol[brute_rol[collum]  == query]
+    if nome_filtro != "":
+        rol_filtrado.to_csv(nome_filtro+".csv",index=False)
+        print(f"TABELA CRIADA: {nome_filtro}")
+    return render_template("dataset.html",table = rol_filtrado.to_html())
